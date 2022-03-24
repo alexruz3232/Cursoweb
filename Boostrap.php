32691a779201c91592bacfb4a7569fa024846132
José <81261923+alexruz3232@users.noreply.github.com>
2022-03-23 23:55:11 -0600

    Estructura de la carpeta autocontenedora del Curso
    Se crea repositorio, se suben archivos principales de Boostrap y JavaScript

---
+++ Boostrap.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible"content="IE=edge">
+    <meta name="viewpoert" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>Instalar Bootstrap5</title>
+  </head>
+  <body>
+    <div class="container" "text-center"></div>
+    <h1>Bootstrap5</h1>
+      <script src="js/bootstrap.min.js"></script>
+
+
+
+  </body>
+</html>
