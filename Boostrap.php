b4008512a862bad21078da480495ae7804e73751
alexruz3232 <81261923+alexruz3232@users.noreply.github.com>
2022-03-24 23:36:01 -0600

    practica1
    recuperando datos y trabajando con operadores aritmeticos y logicos

--- Boostrap.php
+++ Boostrap.php
@@ -8,11 +8,25 @@
     <title>Instalar Bootstrap5</title>
   </head>
   <body>
-    <div class="container" "text-center"></div>
-    <h1>Bootstrap5</h1><hr>
+    <div class="container text-center">
+    <h1>Programando con JavaScript</h1><hr>
+    <form action="">
+      <input type="text" class="form-control" id="objeto1" placeholder="Digita un numero"/>
+    +<input type="text" class="form-control" id="objeto2" placeholder="Digita el segundo numero">
+<input type="submit" value="Calcular" onclick="Calcular();">
+    </form>
+    </div>
+    <script>
+    function Calcular(){
+      numero1 = document.get.ElementById('objeto1').value;
+      numero2 = document.get.ElementById('objeto2').vale;
+      numero1 = parseInt(numero1);
+      numero2 = parseInt(numero2);
+      suma = numero1 + numero2;
+      alert("El resultado es: "+suma);
+
+    }
+    </script>
       <script src="js/bootstrap.min.js"></script>
-
-
-
   </body>
 </html>
